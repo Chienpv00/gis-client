f8cea386e2610f18560988e84dd034a7fb1c722f
chien.pham <chien.pham@spiraledge.com>
2022-10-28 16:54:41 +0700

    crete mockup main navigation

--- src/components/MainNavigation/styled.ts
+++ src/components/MainNavigation/styled.ts
@@ -1,4 +1,12 @@
 import { styled } from '@mui/system';
+import { Button } from '@mui/material';
+
+interface typeButtonCus {
+    bgColor?: string;
+    bgHoverColor?: string;
+    textColor?: string;
+    textHoverColor?: string;
+}
 
 const MyComponent = styled('div')({
     color: 'darkslategray',
@@ -7,4 +15,13 @@ const MyComponent = styled('div')({
     borderRadius: 4,
 });
 
-export { MyComponent };
+const ButtonCus = styled(Button)<typeButtonCus>`
+    background-color: ${(props) => props.bgColor};
+    color: ${(props) => props.textColor};
+
+    &:hover {
+        background-color: ${(props) => props.bgHoverColor};
+        color: ${(props) => props.textHoverColor};
+    }
+`;
+export { MyComponent, ButtonCus };
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from '~/App';
 import reportWebVitals from './reportWebVitals';
 
 import GlobalStyles from './components/GlobalStyles';
+import type {} from '@mui/material/themeCssVarsAugmentation';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
