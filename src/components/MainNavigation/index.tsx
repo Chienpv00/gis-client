3def6e729b4545e95bcce08bc1d32ba0abcff091
chien.pham <chien.pham@spiraledge.com>
2022-10-28 12:53:14 +0700

    finish config

---
+++ src/components/MainNavigation/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { MyComponent } from './styled';
+
+type Props = {};
+
+const MainNavigation = (props: Props) => {
+    return <MyComponent>Styled</MyComponent>;
+};
+
+export default MainNavigation;
---
+++ src/components/MainNavigation/styled.ts
@@ -0,0 +1,10 @@
+import { styled } from '@mui/system';
+
+const MyComponent = styled('div')({
+    color: 'darkslategray',
+    backgroundColor: 'aliceblue',
+    padding: 8,
+    borderRadius: 4,
+});
+
+export { MyComponent };
